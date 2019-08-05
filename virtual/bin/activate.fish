ed866fb596d1fabdb4c66a9d3e04c40954e91ae1
Michelle Mukami <you@example.com>
2019-08-05 07:59:38 +0300

    added installments

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/michelle/pitches/virtual"
+set -gx VIRTUAL_ENV "/home/michelle/Documents/pitches/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
